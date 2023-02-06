c2198d04ee01eab391fc6d36842040da3fceabda
Alexandre Andrade <alex_dev@outlook.com.br>
2023-02-06 12:02:56 -0300

    Finished project

---
+++ backend/src/main/java/com/abdla/crud1/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.abdla.crud1.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Aline', '055594334-89', 5789.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
